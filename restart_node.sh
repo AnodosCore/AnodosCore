f404729facf0deebe46e66b4e510d4a39def597c
Anodos <42297150+AnodosCore@users.noreply.github.com>
2018-08-30 10:32:45 -0400

    Update restart_node.sh

--- restart_node.sh
+++ restart_node.sh
@@ -2,9 +2,7 @@
 cp /usr/local/bin/anodos-cli /root
 cp /usr/local/bin/anodosd /root
 crontab -l > autorestarter
-#echo new cron into cron file
 echo "0 * * * * /root/restart_node.sh" > autorestarter
-#install new cron file
 crontab autorestarter
 mv /root/autorestarter /etc/cron.d
 anodos-cli clearbanned && anodos-cli stop && anodosd -reindex
